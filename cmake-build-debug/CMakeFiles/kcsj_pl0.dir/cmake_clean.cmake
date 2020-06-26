86795ee96bf30b8fd879ce31133ae7f5a85ffd01
zihanPan <470590444@qq.com>
2020-06-26 18:55:05 +0800

    初始化项目

---
+++ cmake-build-debug/CMakeFiles/kcsj_pl0.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/kcsj_pl0.dir/main.c.obj"
+  "kcsj_pl0.exe"
+  "kcsj_pl0.exe.manifest"
+  "kcsj_pl0.pdb"
+  "libkcsj_pl0.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/kcsj_pl0.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/kcsj_pl0.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for kcsj_pl0.
+# This may be replaced when dependencies are built.
